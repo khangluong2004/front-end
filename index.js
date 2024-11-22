c5886874d449a8907d3608a5b5c426c2cf4c6680
khangluong2004 <khangluong2004@gmail.com>
2024-11-22 22:40:15 +1100

    Add balance and reformat the form

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// State values
 var connection = null;
+
 
 // Button functions
 function connect(){
@@ -17,18 +19,8 @@ function connect(){
     connection.onmessage = (event)=>{
         console.log(event.data);
         recordResponse(event.data);
+        recordState(event.data);
     }
-}
-
-function sendEcho(){
-    const msg = {
-        "messageType": document.getElementById("msgType").value, 
-        "ticker": document.getElementById("ticker").value,
-        "price": document.getElementById("price").value
-    }
-
-    connection.send(JSON.stringify(msg));
-    console.log("Finish sending echo");
 }
 
 function retrieveOrder(){
@@ -39,7 +31,60 @@ function retrieveOrder(){
     connection.send(JSON.stringify(msg));
 }
 
+function sendOrder(){
+    const msg = {
+        "messageType": document.getElementById("msgType").value,
+        "userId": document.getElementById("userId").value,
+        "orderId": document.getElementById("orderId").value,
+        "ticker": document.getElementById("ticker").value,
+        "price": parseInt(document.getElementById("price").value),
+        "amount": parseInt(document.getElementById("amount").value),
+        "sell": (document.getElementById("sell").value == "true") ? true:false
+    };
+
+    console.log(msg);
+
+    connection.send(JSON.stringify(msg));
+}
+
 // Listening events support functions
 function recordResponse(data){
     document.getElementById("response").innerHTML = data;
 }
+
+function recordState(dataStr){
+    let data = JSON.parse(dataStr)
+    if (!("messageType" in data)){
+        return;
+    }
+
+    let type = data["messageType"];
+    
+    if (type == "getBalance"){
+        document.getElementById("myBalance").innerHTML = data["value"];
+    }
+}
+
+// Interval polling
+function getBalance(){
+    if (connection == null){
+        return;
+    }
+
+    let userId = document.getElementById("userId");
+    if (userId == null || userId.value == ""){
+        return;
+    }
+
+    msg = {
+        "messageType": "getBalance",
+        "userId": userId.value
+    }
+
+    // console.log(msg);
+    connection.send(JSON.stringify(msg));
+}
+
+window.setInterval(() => {
+    getBalance();
+}, 1000);
